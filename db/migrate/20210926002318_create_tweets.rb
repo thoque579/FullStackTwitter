607f92f208e7760b2a90e404d488bdc1a701ab55
thoque579 <tusharhoque2gmail.com>
2021-09-25 20:43:40 -0400

    added structure to database

--- db/migrate/20210926002318_create_tweets.rb
+++ db/migrate/20210926002318_create_tweets.rb
@@ -1,7 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string :message
+      t.belongs_to :user, index: true, foreign_key: true
       t.timestamps
     end
   end
