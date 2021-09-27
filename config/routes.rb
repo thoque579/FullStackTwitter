2d74f04d99b8c1d6a639d141d26a3b5356fffa7b
thoque579 <tusharhoque2gmail.com>
2021-09-26 21:01:54 -0400

    added and finished login and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get '/signup' => 'static_pages#signup'
   get '/login' => 'static_pages#login'
+  get '/feeds' => 'static_pages#feeds'
   namespace :api do
     #user
     post '/user' => 'users#create'
