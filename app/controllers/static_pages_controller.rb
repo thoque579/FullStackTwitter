b3f453b48fcccecc0df87816670ebd5d2d37721d
tusharhoque2@gmail.com <tusharhoque2gmail.com>
2021-11-06 16:17:49 -0400

    revert

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,6 @@ class StaticPagesController < ApplicationController
   end
 
   def feeds
-    token = cookies.signed[:twitter_session_token]
-    session = Session.find_by(token: token)
-    @current_user = session.user.username
     render 'feeds'
   end
 
