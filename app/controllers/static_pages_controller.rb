37fc7d25f4c54a9bd439ef22006ff1e25be60742
tusharhoque2@gmail.com <tusharhoque2gmail.com>
2021-11-06 15:10:07 -0400

    added icons

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,11 @@ class StaticPagesController < ApplicationController
   def signup
     render 'signup'
   end
-  
+
   def feeds
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+    @current_user = session.user.username
     render 'feeds'
   end
 
